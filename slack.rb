b0c3c0eed56e77607312f6fffcf704996809d121
sato kazuki <dracrowa@gmail.com>
2016-09-28 22:19:48 +0100

    リファクタリング

--- slack.rb
+++ slack.rb
@@ -7,6 +7,7 @@ setting = YAML.load_file("settings.yml")
 context = Mysql2::Client.new(:host => "localhost", :username => "root", :password => setting["password"], :database => "slack", encoding: 'utf8')
 TOKEN = setting["token"]
 
+BOTNAME = "task-manager"
 IMAGEURL = 'http://i15.photobucket.com/albums/a383/SeanyBoyo/Celebrities/Audrey%20Hepburn/1893337_029.png'
 Slack.configure {|config| config.token = TOKEN }
 
@@ -19,48 +20,35 @@ end
 client.on :message do |data|
   if data.key?('text')
         if  data['text'].include?('残作業') && data['user'] != nil
-                puts data['user']
                 place = data['text'].index('残作業') + 7
                 target = data['text'][place..data['text'].length]
-                puts data['user']
-                puts target
-                params = {
-                        channel: data['channel'],
-                        username: "task-manegar",
-                        text:  "<@#{data['user']}> your task was memorized"
-                }
-                #Slack.chat_postMessage params
                 query = ("insert into remaining_tasks (user_id,task_text) values ('#{data['user']}','#{target}')")
                 results = context.query(query)
-                query = ("select * from remaining_tasks where user_id = '#{data['user']}' order by id desc limit 1")
-                results = context.query(query)
-                puts query
-                results.each do |row|
-                        puts row['user_id']
-                        puts row['task_text']
-                end
+                p "add column #{target}"
         end
-        if data['text'] == "tell my task"
 
+        if data['text'] == "@maid-bot task"
                 query = ("select * from remaining_tasks where user_id = '#{data['user']}' order by id desc limit 1")
                 results = context.query(query)
                 puts query
                 results.each do |row|
                         puts row['user_id']
                         puts row['task_text']
-
-                params = {
-                        channel: data['channel'],
-                        username: "task-manager" ,
-                        text:  "<@#{data['user']}>\n 作業を開始します。\n<残作業>#{row['task_text']} ",
-                        icon_url: IMAGEURL
-                }
-
+                        params = createParam(row['task_text'])
                 end
-                          
                 Slack.chat_postMessage params
         end
   end
+end
+
+def createParam(message)
+    params = {
+        channel: data['channel'],
+        username: BOTNAME ,
+        text:  message
+        icon_url: IMAGEURL
+    }
+    return params
 end
 
 client.start
